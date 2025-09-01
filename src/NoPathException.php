d7543a12f3f74c3d7c63d1ea6a98e8bc02a9e6e0
Marcos <lilianemarcos2018@gmail.com>
2025-09-01 19:49:18 -0300

    Add NoPathException class for handling path-related errors

---
+++ src/NoPathException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Src;
+
+class NoPathException extends \Exception
+{
+    public function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString(): string
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
